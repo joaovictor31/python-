d0f2d99c4270cd103b612b730d1dd1f251c56382
João <ut7vitor60@gmail.com>
2024-05-23 16:46:38 -0300

    inicio do exercício de array

---
+++ JS/arrays2.js
@@ -0,0 +1,7 @@
+const cursos = document.querySelectorAll('.curso');
+//Aqui o resultado está em node list e tenho que passar para array
+const arraycursos = Array.from(cursos);
+
+
+console.log(cursos);
+console.log(arraycursos);
---
+++ repeticao.py
@@ -0,0 +1,6 @@
+cont = 0
+while cont < 10:
+    cont+=1
+
+
+print(cont)
